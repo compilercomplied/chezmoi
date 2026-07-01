5851987f8267f6c4f5ef6596a0c5ad7fb5012e4c
compilercomplied <8614035+compilercomplied@users.noreply.github.com>
2026-07-01 10:48:42 +0200

    dark/light sync with nvim

--- dot_config/sketchybar/plugins/executable_theme_change.sh
+++ dot_config/sketchybar/plugins/executable_theme_change.sh
@@ -14,4 +14,16 @@ LAST_THEME=$(cat /tmp/sketchybar_theme 2>/dev/null)
 if [ "$CURRENT_THEME" != "$LAST_THEME" ]; then
   echo "$CURRENT_THEME" > /tmp/sketchybar_theme
   sketchybar --reload
+
+  # Sync Neovim theme
+  theme_lower=$(echo "$CURRENT_THEME" | tr '[:upper:]' '[:lower:]')
+  echo "$theme_lower" > "$HOME/.config/nvim/.appearance"
+
+  # Signal all running Neovim instances via their sockets
+  for socket in $(lsof -a -U -c nvim | awk '$NF ~ /^\// {print $NF}' 2>/dev/null); do
+    if [ -S "$socket" ]; then
+      nvim --server "$socket" --remote-send "<Cmd>set background=$theme_lower<CR>" 2>/dev/null &
+    fi
+  done
 fi
+
